794b3b375ec943f70db9909f9df5933933ae3fe9
Qhama Hlana <queartstudio3@gmail.com>
2026-09-19 21:59:34 +0200

    ;rgb:0000/0000/0000
    This reverts commit 430d584c960a7c0d8a116be0ed364e6e00ac2348.

--- src/app/(tabs)/map.tsx
+++ src/app/(tabs)/map.tsx
@@ -761,7 +761,6 @@ const styles = StyleSheet.create({
   roundBtn2: {
     width: 44,
     height: 44,
-    top: 280,
     borderRadius: 22,
     alignItems: "center",
     justifyContent: "center",
